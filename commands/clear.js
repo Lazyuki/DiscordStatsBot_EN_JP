2152d4462da4631709c53736a2758c16b4792091
Yuki Ito <yukiito@uw.edu>
2017-06-27 03:52:48 -0700

    fixed clear command

--- commands/clear.js
+++ commands/clear.js
@@ -5,10 +5,10 @@ module.exports.alias = [
 
 module.exports.command = async (message, content, bot) => {
   let chan = message.channel;
-	let messages = await chan.fetchMessages({limit : 20});
-	for (var m in messages) {
-		if (messages.get(m).author.id == bot.user.id) {
-			messages.get(m).delete();
+	var messages = await chan.fetchMessages({limit:30});
+	for (var m of messages.values()) {
+		if (m.author.id == bot.user.id) {
+			m.delete();
 		}
 	}
-}
+};
