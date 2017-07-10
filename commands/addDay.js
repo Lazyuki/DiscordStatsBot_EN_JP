d3125e8639dff027f2e30aff45515be214df8d9a
Yuki Ito <yukiito@uw.edu>
2017-07-11 01:27:02 +0900

    implemented addDay

---
+++ commands/addDay.js
@@ -0,0 +1,10 @@
+module.exports.alias = [
+  'addDay',
+  'addday'
+];
+
+module.exports.command = (message, content, bot) => {
+  if (message.author.id != bot.owner_ID) return;
+  bot.server.today = (bot.server.today + 1) % 31;
+  message.channel.send(`Day is now ${bot.server.today}`);
+};
