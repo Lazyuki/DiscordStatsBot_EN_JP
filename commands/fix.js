fc5ab334c7d16d57bcdbbf51e3ea7d312c04a55d
Yuki Ito <yukiito@uw.edu>
2017-09-01 16:08:36 +0900

    realfix fix

--- commands/fix.js
+++ commands/fix.js
@@ -1,5 +1,5 @@
 module.exports.alias = [
-	'realfixSept6'
+	'realfix'
 ];
 
 module.exports.command = (message, content, bot, server) => {
@@ -8,10 +8,11 @@ module.exports.command = (message, content, bot, server) => {
 		let u = server.users[user];
 		var realJP = 0;
 		for (var day in u.record) {
-			if (u.record[day]['jpn']) {
+			if (u.record[day] && u.record[day]['jpn']) {
 				realJP += u.record[day]['jpn'];
 			}
 		}
 		u.jp = realJP;
 	}
+	message.channel.send('done');
 };
