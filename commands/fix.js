09dc0c26838ebb85723babc01624518153a1deb5
Yuki Ito <yukiito@uw.edu>
2017-09-20 15:30:07 +0900

    japanese fix?

--- commands/fix.js
+++ commands/fix.js
@@ -6,13 +6,12 @@ module.exports.command = (message, content, bot, server) => {
 	if (message.author.id != bot.owner_ID) return;
 	for (var user in server.users) {
 		let u = server.users[user];
-		var count = 0;
-		for (var ch in u.chans) {
-			if (u.chans[ch] == null) {
-				delete server.users[user].chans[ch];
-				count++;
+		var realJP = 0;
+		for (var day in u.record) {
+			if (u.record[day] && u.record[day]['jpn']) {
+				realJP += u.record[day]['jpn'];
 			}
 		}
+		u.jp = realJP;
 	}
-	message.channel.send(count);
-};
+	message.channel.send('done');
