37258c331c9bf3c14a37b615d1e25f6d6d46c907
Yuki Ito <yukiito@uw.edu>
2017-11-02 22:43:30 -0700

    permission revert

--- commands/muteNewUsers.js
+++ commands/muteNewUsers.js
@@ -5,15 +5,13 @@ module.exports.alias = [
 module.exports.command = async (message, content, bot, server) => {
   if (!message.member.hasPermission('ADMINISTRATOR')) return;
 	let nu = server.guild.roles.get('249695630606336000'); // New User
-	if (nu.hasPermission('SEND_MESSAGES')) { // Get rid of all permissions.
-		nu.setPermissions();
-		server.guild.defaultRole.setPermissions();
+	nu.setPermissions('ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES',
+	'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME');
+	server.guild.defaultRole.setPermissions('ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES',
+	'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME');
+	if (!nu.hasPermission('SEND_MESSAGES')) { // Get rid of all permissions.
 		message.channel.send('New Users are now *muted*. Please type the same command again once the raid is done.');
 	} else { // restore the old state
-		nu.setPermissions('ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES',
-		'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME');
-		server.guild.defaultRole.setPermissions('ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES',
-		'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME');
 		message.channel.send('New Users are now *unmuted*.');
 	}
 };
