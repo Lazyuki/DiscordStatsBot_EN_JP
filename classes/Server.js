1447e7aa7c113ea57413e90fd6774af61830bc95
Yuki Ito <yukiito@uw.edu>
2017-11-04 22:04:01 -0700

    full width restriction

--- classes/Server.js
+++ classes/Server.js
@@ -16,7 +16,7 @@ const LINEclient = new LINE.Client({
 
 // Langex regex
 const jpregex = /[\u3040-\u30FF]|[\uFF66-\uFF9D]|[\u4E00-\u9FAF]/;
-const enregex = /[a-zA-Z]/;
+const enregex = /[a-zA-Z]|[Ａ-ｚ]/;
 
 
 module.exports = class Server {
