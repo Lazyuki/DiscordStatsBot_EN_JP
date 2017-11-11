a7c4e9517f3254a563d512452cbc73d9dd7bc8f0
Yuki Ito <yukiito@uw.edu>
2017-11-10 17:31:13 -0800

    help

--- commands/help.js
+++ commands/help.js
@@ -7,12 +7,17 @@ module.exports.alias = [
 
 module.exports.command = (message, content, bot, server) => {
   let chan = message.channel;
-	chan.send(
-		'Current commands are \n\`leaderboard (lb)\`\n\`channel-leaderboard (chlb)\`\n\`user (u)\`\n\`channels (ch)\`\n\`snowflake (id)\`\n\`clear (clr)\`');
+	var msg = '**Commands:**\n\`leaderboard (lb)\`\n\`channel-leaderboard (chlb)\`\n\`user (u)\`\n\`channels (ch)\`\n\`snowflake (id)\`\n\`clear (clr)\`\n';
+	if (message.member.hasPermission('MANAGE_ROLES')) {
+		msg += '**Welcoming Party Commands:**\n\`tag (t)\`: `,t <nj | fj | ne | fe | ol> [@someone, or 1, 2, 3]` See the pin in <#277384105245802497>\n';
+		msg += '\`nofilter (nf)\`: sends people to ~~oblivion~~ <#193966083886153729> for 5 minutes. \`,nf @someone @sometwo @somethree\`\n';
+	}
 	if (message.member.hasPermission('ADMINISTRATOR') && server.hiddenChannels.includes(message.channel.id)) {
-		chan.send('Current mod commands are \n\`hide\`, \`hidden\`, and \`unhide\` for hiding channels (use IDs).\n' +
+		msg += '**Mod Commands** \n\`hide\`, \`hidden\`, and \`unhide\` for hiding channels (use IDs).\n' +
 			'\`watch\`, \`watched\`, and \`unwatch\` for tracking deleted messages of a specific user. Use ID or mention. Names won\'t work. \n' +
 		  '\`mutenew\` mutes new users in text chat. do the same command again to disable. **Intended for raids**\n' +
-			'\`auditlog\` shows the last 5 interesting audit logs. ');
+			'\`auditlog (al)\` shows the last 5 interesting audit logs.';
 	}
+	chan.send(msg);
+
 };
