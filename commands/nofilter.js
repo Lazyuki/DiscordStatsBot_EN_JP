5d84e59632bfc1472798e5d6c78167f906b0555d
Yuki Ito <yukiito@uw.edu>
2017-11-15 15:14:18 -0800

    moves VC with nf

--- commands/nofilter.js
+++ commands/nofilter.js
@@ -1,6 +1,7 @@
 const nofilterOnlyRole = '378668720417013760';
 const nofilterRole = '196106229813215234';
 const nofilterChan = '193966083886153729';
+const nofilterVoice = '196684007402897408';
 
 module.exports.alias = [
 	'nofilter',
@@ -27,6 +28,7 @@ module.exports.command = async (message, content, bot, server) => {
   for (var mem of members) {
     mem.addRole(nofilterOnlyRole);
     mem.addRole(nofilterRole);
+		mem.setVoiceChannel(nofilterVoice);
     forlater.push(mem);
     names += mem + ' ';
   }
