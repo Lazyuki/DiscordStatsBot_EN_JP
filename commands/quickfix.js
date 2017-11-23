f14b1c6f74cf8038fbf21ec3ac69f2573ea48cba
Yuki Ito <yukiito@uw.edu>
2017-11-22 18:01:03 -0800

    sort kanjis

--- commands/quickfix.js
+++ commands/quickfix.js
@@ -6,7 +6,15 @@ module.exports.command = (message, content, bot, server) => {
 	if (message.author.id != bot.owner_ID) return;
 	switch (content) {
 		case 'list':
-			message.channel.send(JSON.stringify(server.kanjis));
+			// Sorts the kanjis
+		  var sortable = [];
+			for (var k in server.kanjis) {
+			  sortable.push([k, server.kanjis[k]]);
+			}
+			sortable.sort(function(a, b) {
+			  return b[1] - a[1];
+			});
+			message.channel.send(JSON.stringify(sortable));
 			break;
 		default:
 			server.kanjiCheck = !server.kanjiCheck;
