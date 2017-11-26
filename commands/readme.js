8ff9b0d62a553b38e18f7c20c2e966e1503c743c
Yuki Ito <yukiito@uw.edu>
2017-11-26 07:06:26 -0800

    readme fix

--- commands/readme.js
+++ commands/readme.js
@@ -8,11 +8,11 @@ module.exports.command = (message) => {
 	let embed = new Discord.RichEmbed();
 	embed.title = '__**WELCOME!! :tada: READ ME!**__';
 	embed.setImage('https://i.imgur.com/snInv3M.png');
-	embed.addField('Ask Japanese Questions', '#japanese_questions', true);
-	embed.addField('Beginner Japanese Chat', '#beginner_jpn_chat', true);
-	embed.addField('Answer English Questions', '#english_questions', true);
-	embed.addField('Language Exchange', '#language_exchange', true);
-	embed.addField('Server Rules', 'Please read #server_rules for more info', true);
+	embed.addField('Ask Japanese Questions', '<#189601264424714241>', true);
+	embed.addField('Beginner Japanese Chat', '<#208118574974238721>', true);
+	embed.addField('Answer English Questions', '<#193959229030268938>', true);
+	embed.addField('Language Exchange', '<#376574779316109313>', true);
+	embed.addField('Server Rules', 'Please read <#189585230972190720> for more info', true);
 	embed.color = 16711935;
 	message.channel.send({embed});
 };
--- commands/yonde.js
+++ commands/yonde.js
@@ -8,11 +8,11 @@ module.exports.command = (message) => {
 	let embed = new Discord.RichEmbed();
 	embed.title = '__ようこそ！！ :tada: このサーバーの簡単な説明__';
 	embed.setImage('https://i.imgur.com/56C1pjx.png');
-	embed.addField('日本語で雑談', '#japanese_chat', true);
-	embed.addField('英語で雑談', '#language_exchange', true);
-	embed.addField('英語で言語交換', '#language_exchange', true);
-	embed.addField('英語に関する質問', '#english_questions', true);
-	embed.addField('その他ルール', '#server_rules の下の方に日本語で説明があります', true);
+	embed.addField('日本語で雑談', '<#189629338142900224>', true);
+	embed.addField('英語で雑談', '<#234772801716879371>', true);
+	embed.addField('英語で言語交換', '<#376574779316109313>', true);
+	embed.addField('英語に関する質問', '<#193959229030268938>', true);
+	embed.addField('その他ルール', '<#189585230972190720> の下の方に日本語で説明があります', true);
 	embed.color = 16711935;
 	message.channel.send({embed});
 }
