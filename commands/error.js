e23899246a250154dd362e569fb9b3a916b92b1f
Yuki Ito <yukiito@uw.edu>
2017-12-03 13:10:03 -0800

    error command

---
+++ commands/error.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const nohup = './nohup.out';
+
+module.exports.alias = [
+	'error'
+];
+
+module.exports.command = (message, content, bot) => {
+	if (message.author.id != bot.owner_ID) return;
+	if (content == 'reset') {
+		fs.writeFile(nohup, '', function (err) {
+			if (err) {
+				message.channel.send('write failed: ' + err);
+			};
+		})
+	} else {
+		fs.readFile(nohup, 'utf8', function (err, data) {
+			if (err) {
+				message.channel.send('read failed: ' + err);
+				return;
+			};
+			if (data.length > 2000) {
+				message.channel.send(`\`\`\`${data.substr(data.length - 1990)}\`\`\``)
+			} else {
+				message.channel.send(`\`\`\`${data ? data : 'Empty'}\`\`\``);
+			}
+		});
+
+	}
+};
