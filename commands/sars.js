799a28efd33ac1b5a183902d6fbe967448f3f9f2
Yuki Ito <yukiito@uw.edu>
2017-12-04 08:05:10 -0800

    auto react

--- commands/sars.js
+++ commands/sars.js
@@ -6,19 +6,22 @@ module.exports.alias = [
 
 module.exports.command = async (message, content, bot, server) => {
 	if (!message.member.hasPermission('ADMINISTRATOR')) return;
-	let str = 'React with those emojis to toggle the role.\n'
+	let str = 'React with those emojis to toggle the roles.\n'
 	let sortable = [];
 	for (let emoji in server.sars) {
 		let role = server.guild.roles.get(server.sars[emoji]);
 		if (!role) continue;
-		sortable.push([role.name, `${emoji} => **${role.name}**\n`]);
+		sortable.push([role.name, emoji]);
 	}
 	// Sorts roles
 	sortable.sort(function(a, b) {
 			return a[0] < b[0] ? -1 : (a[0] > b[0] ? 1 : 0);
 	});
 	for (let i in sortable) {
-		str += sortable[i][1];
+		str += `${sortable[i][1]} => **${sortable[i][0]}**\n`;
 	}
-	message.channel.send(str);
+	let msg = await message.channel.send(str);
+	for (let i in sortable) {
+		await msg.react(sortable[i][1]);
+	}
 };
