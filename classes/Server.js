8bb06c14e09c9c7318d809467c5093fe56b8572d
Yuki Ito <yukiito@uw.edu>
2017-12-07 11:26:14 -0800

    fetch before adding messsage

--- classes/Server.js
+++ classes/Server.js
@@ -69,6 +69,7 @@ module.exports = class Server {
     }
 
     addMessage(message) {
+      await message.guild.fetchMember(message.author);
       let author = message.author.id;
       let channel = message.channel.id;
       if (!this.users[author]) {
@@ -271,6 +272,7 @@ module.exports = class Server {
     }
 
     addEdits(oldMessage, newMessage) {
+      await newMessage.guild.fetchMember(newMessage.author);
       if (newMessage.channel.id == '376574779316109313') this.checkLanEx(newMessage); // Check language exchange.
       if (newMessage.channel.id == '208118574974238721') this.checkBegJp(newMessage); // Check beginner jpn chat
       if (!newMessage.member) {
