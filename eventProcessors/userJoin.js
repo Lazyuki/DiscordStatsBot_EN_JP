ffc93528af58d998b196635297f2a8ac096a1d04
Yuki Ito <yukiito@uw.edu>
2018-01-03 15:07:25 -0800

    log

--- eventProcessors/userJoin.js
+++ eventProcessors/userJoin.js
@@ -4,8 +4,8 @@ module.exports.actions = ['JOIN'];
 module.exports.initialize = (json, server) => {
   server.newUsers = [];
   if (!json || !json['newUsers']) return;
-  console.log(json['newUsers']);
   server.newUsers = json['newUsers'];
+  console.log(server.newUsers);
 };
 module.exports.isAllowed = () => {
   return true;
@@ -14,4 +14,5 @@ module.exports.isAllowed = () => {
 module.exports.process = async (memberID, server) => {
   console.log(server.newUsers);
   if (server.newUsers.push(memberID) > 3) server.newUsers.shift();
+  console.log(server.newUsers);
 };
