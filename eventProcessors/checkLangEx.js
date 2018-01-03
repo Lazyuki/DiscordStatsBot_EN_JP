d1d390ab80aee3fea590d6f69cac9044932e6bd8
Yuki Ito <yukiito@uw.edu>
2018-01-03 13:09:08 -0800

    put else

--- eventProcessors/checkLangEx.js
+++ eventProcessors/checkLangEx.js
@@ -10,12 +10,11 @@ const Util = require('../classes/Util.js');
 
 module.exports.process = (message, server, bot, language) => {
   let isJapanese = message.member.roles.has('196765998706196480'); // has native japanese
-  if ((language & Util.LANG.ESC) || (language & Util.LANG.OTH)) { // if escape or other.
-    for (var r of message.reactions.values()) {
-      if (r.me) r.remove();
-    }
-  }
   if (((language & Util.LANG.JPN) && isJapanese) || ((language & Util.LANG.ENG) && !isJapanese)) {
     message.react('🚫');
+  } else if ((language & Util.LANG.ESC) || (language & Util.LANG.OTH)) { // if escape or other.
+    for (let r of message.reactions.values()) {
+      if (r.me) r.remove();
+    }
   }
 };
