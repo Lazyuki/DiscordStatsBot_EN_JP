e3c4a9ba43c2759f86d3b3c35d6d71fce604cab1
Yuki Ito <yukiito@uw.edu>
2018-01-16 17:10:55 -0800

    joke command kawaii

---
+++ commands/kawaii.js
@@ -0,0 +1,21 @@
+module.exports.name = 'kawaii';
+
+module.exports.alias = [
+  'kawaii'
+];
+
+
+module.exports.isAllowed = (message, server) => {
+  return server.guild.id == '189571157446492161';
+};
+
+module.exports.help = 'Shows who is the kawaiiest.';
+
+const Discord = require('discord.js');
+
+module.exports.command = (message) => {
+  let embed = new Discord.RichEmbed();
+  embed.setImage('https://i.imgur.com/hRBicd2.png');
+  embed.color = 16753111;
+  message.channel.send({embed});
+};
