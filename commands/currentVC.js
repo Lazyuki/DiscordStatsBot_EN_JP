dd91d0f5e8bcd8c68431e850b23f94bcb55266ca
Yuki Ito <yukiito@uw.edu>
2018-02-14 10:50:27 -0800

    currentVC

---
+++ commands/currentVC.js
@@ -0,0 +1,20 @@
+module.exports.name = 'currentVC';
+
+module.exports.alias = [
+  'cvc'
+];
+
+module.exports.isAllowed = (message, server, bot) => {
+  if (message.guild.id != '293787390710120449') return false;  // My server  
+  return message.author.id == bot.owner_ID;
+};
+
+module.exports.help = 'Save the server state.';
+
+module.exports.command = async (message, _, bot, server) => {
+  for (var v in server.tempvc) {
+    let t = server.tempvc[v];
+    let member = await server.guild.fetchMember(v);
+    console.log(`${member.user.username}: ${Math.floor((t - new Date().getTime()) / 60000)} min`);
+  }
+};
--- commands/save.js
+++ commands/save.js
@@ -12,7 +12,6 @@ module.exports.isAllowed = (message, server, bot) => {
 module.exports.help = 'Save the server state.';
 
 module.exports.command = (message, _, bot) => {
-  if (message.author.id != bot.owner_ID) return;
   for (var s in bot.servers) {
     let server = bot.servers[s];
     server.save();
