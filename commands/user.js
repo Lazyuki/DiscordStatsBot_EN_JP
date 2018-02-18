01f1c3e048b8d478e4c960d32a28a0909b9d5c21
Yuki Ito <yukiito@uw.edu>
2018-02-18 12:10:37 -0800

    roll back

--- commands/user.js
+++ commands/user.js
@@ -109,7 +109,6 @@ module.exports.command = async (message, content, bot, server) => {
     embed.color = fire ? Number('0xFF5500') : Number('0x3A8EDB');
     embed.setFooter('Joined this server');
     embed.timestamp = member.joinedAt;
-    console.log(member.joinedAt);
   } else { // user left
     embed.setAuthor(`Stats for <@${content}>`);
     embed.color = Number('0x3A8EDB');
