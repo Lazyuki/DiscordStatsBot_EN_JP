831d02daa912464b7236fdca31d484baf279abbc
Yuki Ito <yukiito@uw.edu>
2018-03-07 21:33:52 -0800

    fix mute new users feature

--- commands/muteNewUsers.js
+++ commands/muteNewUsers.js
@@ -10,18 +10,19 @@ module.exports.isAllowed = (message, server) => {
 };
 
 module.exports.help = '__Mods Only__ Mute new users in text chat. Type the same command again to disable it. **Intended for raids**';
+const mutedPerms = ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'];
+const minPerms = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'];
+const normalPerms = ['ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME'];
 
 module.exports.command = async (message, content, bot, server) => {
   let nu = server.guild.roles.get('249695630606336000'); // New User
   if (nu.hasPermission('SEND_MESSAGES')) { // Get rid of all permissions.
-    nu.setPermissions(['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY']);
-    server.guild.defaultRole.setPermissions(['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY']);
-    message.channel.send('New Users are now *muted*. Please type the same command again once the raid is over.');
+    nu.setPermissions(mutedPerms);
+    server.guild.defaultRole.setPermissions(mutedPerms);
+    message.channel.send('New Users are now *muted*. YOU MUST type the same command again once the raid is over.');
   } else { // restore the old state
-    nu.setPermissions(['ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'ATTACH_FILES',
-      'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME']);
-    server.guild.defaultRole.setPermissions(['ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'ATTACH_FILES',
-      'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS', 'CHANGE_NICKNAME']);
+    nu.setPermissions(normalPerms);
+    server.guild.defaultRole.setPermissions(minPerms);
     message.channel.send('New Users are now *unmuted*.');
   }
 };
