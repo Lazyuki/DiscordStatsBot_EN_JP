7dabd52055fe9df747fbf250a7ee47843d95e80e
Yuki Ito <yukiito@uw.edu>
2018-03-13 12:01:19 -0700

    welcoming party stats

---
+++ commands/wps.js
@@ -0,0 +1,23 @@
+module.exports.name = 'welcomingParties';
+module.exports.alias = [
+  'wp'
+];
+
+module.exports.isAllowed = (message, server) => {
+  return true;
+};
+
+module.exports.help = 'Welcoming party stats';
+
+module.exports.command = (message, content, bot, server) => {
+  let wps = server.guild.members.filter((m) => {return m.hasRole('250907197075226625');});
+  let str = '';
+  for (let [id, wp] of wps) {
+    if (server.users[id]) {
+      str += `${wp.user.tag} : ${server.users[id].thirty}\n`;
+    } else {
+      str += `${wp.user.tag} : 0\n`;
+    }
+  }
+  message.channel.send(str);
+};
