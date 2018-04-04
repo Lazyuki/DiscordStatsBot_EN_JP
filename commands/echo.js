114785e111700757bf8be4d00063d63509bd9b30
Yuki Ito <yukiito@uw.edu>
2018-04-04 10:59:16 -0700

    echo to everyone

--- commands/echo.js
+++ commands/echo.js
@@ -5,11 +5,12 @@ module.exports.alias = [
 ];
 
 module.exports.isAllowed = (message, server, bot) => {
-  if (message.guild.id != '293787390710120449') return false;  // My server  
-  return message.author.id == bot.owner_ID;
+  //if (message.guild.id != '293787390710120449') return false;  // My server  
+  // return message.author.id == bot.owner_ID;
+  return true;
 };
 
-module.exports.help = '`,echo <things to say>` Repeat what you\'ve said, normally and inside a code block.';
+module.exports.help = '`,echo <things to say>` Repeat what you\'ve said, as a plain text and inside a code block.';
 
 module.exports.command = (message, content) => {
   message.channel.send(content);
