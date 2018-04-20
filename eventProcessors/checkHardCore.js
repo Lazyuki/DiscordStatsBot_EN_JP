f763183e9134cabd7a8abe62634b93321730b848
Yuki Ito <yukiito@uw.edu>
2018-04-20 15:58:47 -0700

    ignore bot-quiz

--- eventProcessors/checkHardCore.js
+++ eventProcessors/checkHardCore.js
@@ -21,7 +21,7 @@ module.exports.process = (message) => {
   let content = message.content;
   let isJapanese = message.member.roles.has('196765998706196480') || message.member.roles.has('292401145752846337'); // has native japanese  
   if (message.channel.id == '225828894765350913' && /^(k!|t!|[!.&%=+$])/.test(content)) return; // #bot
-  if (message.channel.id == '343151415595892739' && /^(k!|t!|kq!|m!|[!<.])/.test(content)) return; // #bot-spam
+  if (['343151415595892739', '343057592467062795', '304758913914503168', '344541594755792896'].includes(message.channel.id) && /^(k!|t!|kq!|m!|[!<.])/.test(content)) return; // #bot-spam
 
   if (/^\.\.\.\s[\S]+$/.test(content)) return; // nadeko quote
   if (!isJapanese) { // for welcoming
