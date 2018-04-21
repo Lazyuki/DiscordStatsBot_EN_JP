491c7d4909ff9206ecde3a70c4b0f0b155eef3bb
Yuki Ito <yukiito@uw.edu>
2018-04-20 23:13:23 -0700

    test readme

--- commands/readme.js
+++ commands/readme.js
@@ -16,13 +16,13 @@ const Discord = require('discord.js');
 module.exports.command = (message) => {
   let embed = new Discord.RichEmbed();
   embed.title = '__**WELCOME!! :tada: READ ME!**__';
-  embed.description = '__**[Resources for learning Japanese<:externallink:437129837149159435>](https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md)**__';
+  embed.description = '__**[Japanese Starting Guide<:externallink:437129837149159435>](https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md)**__';
   embed.setImage('https://i.imgur.com/Wzy5eQD.png');
   //embed.addField('Ask Japanese Questions', '<#189601264424714241>', true);
   //embed.addField('Beginner Japanese Chat', '<#208118574974238721>', true);
   //embed.addField('Answer English Questions', '<#193959229030268938>', true);
   //embed.addField('Language Exchange', '<#376574779316109313>', true);
-  embed.addField('\u200B', '**Please read <#189585230972190720> carefully!**', false);
+  embed.addField('**↓Rules↓ ↑Link to Resources↑**', '<#189585230972190720>', false);
   embed.color = 16711935;
   message.channel.send({embed});
 };
