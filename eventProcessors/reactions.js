7ff4551da23164aa5823d22e92829506a30eb5d3
Yuki Ito <yukiito@uw.edu>
2018-04-22 03:40:34 -0700

    fix

--- eventProcessors/reactions.js
+++ eventProcessors/reactions.js
@@ -11,7 +11,7 @@ module.exports.process = async (reaction, user, added, server) => {
   let record = server.users[user.id];
   if (!record) return; // the user has no record
   console.log(`name: ${reaction.emoji.name} id: ${reaction.emoji.id} ident: ${reaction.emoji.identifier} str: ${reaction.emoji}`);
-  reaction.message.channel.send(`${reaction.emoji.name} ${reaction.emoji.id} ${reaction.emoji.identifier} ${reaction.emoji}`);
+  // reaction.message.channel.send(`${reaction.emoji.name} ${reaction.emoji.id} ${reaction.emoji.identifier} ${reaction.emoji}`);
   return;
   if (added) {
     record.addReacts(reaction.toString(), server.today);
