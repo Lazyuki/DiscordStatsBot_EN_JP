1b4fc20e9ef619a72f4bfb667ada5ad86c472175
Yuki Ito <yukiito@uw.edu>
2018-04-24 07:34:57 -0700

    formatting

--- commands/guide.js
+++ commands/guide.js
@@ -19,19 +19,19 @@ module.exports.command = (message, content) => {
   let embed = new Discord.RichEmbed();
   content = content.toLowerCase();
   if (content == 'guide') {
-    embed.title = 'Guides <:externallink:437129837149159435>';
+    embed.title = '__**Guides <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#beginner-guide';
     embed.description = 'All beginners should read one of the below guides';
     embed.addField('r/LearnJapanese\'s guide', 'https://www.reddit.com/r/LearnJapanese/wiki/index/startersguide', false);
     embed.addField('A well written DJT guide', 'https://djtguide.neocities.org/guide.html', false);
   } else if (content == 'kana') {
-    embed.title = 'Kana <:externallink:437129837149159435>';
+    embed.title = '__**Kana <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#hiragana-and-katakana';
     embed.description = 'The first step is learning Hiragana and Katakana.';
     embed.addField('Tofugu Hiragana guide', 'https://www.tofugu.com/japanese/learn-hiragana/', false);
     embed.addField('Tofugu Katakana guide', 'https://www.tofugu.com/japanese/learn-katakana/', false);
   } else if (content == 'kanji') {
-    embed.title = 'Kanji <:externallink:437129837149159435>';
+    embed.title = '__**Kanji <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#kanji';
     embed.description = 'After you\'ve got your kana down, you need Kanji';
     embed.addField('Anki decks', 'https://djtguide.neocities.org/anki.html', true);
@@ -39,34 +39,34 @@ module.exports.command = (message, content) => {
     embed.addField('Kanji Koohi', 'https://kanji.koohii.com/', true);
     embed.addField('Kanji Damage', 'http://www.kanjidamage.com/introduction', true);
   } else if (content == 'grammar') {
-    embed.title = 'Grammar <:externallink:437129837149159435>';
+    embed.title = '__**Grammar <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#grammar';
     embed.description = 'Either use structured textbooks or online grammar guides';
     embed.addField('Genki: Beginner textbook', 'http://genki.japantimes.co.jp/index_en', false);
     embed.addField('Tobira: Intermediate textbook', 'http://tobiraweb.9640.jp/', false);
     embed.addField('Tae-Kim: Online guide', 'http://www.guidetojapanese.org/learn/grammar', false);
   } else if (content == 'vocab') {
-    embed.title = 'Vocabulary <:externallink:437129837149159435>';
+    embed.title = '__**Vocabulary <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#vocabulary';
     embed.description = 'For vocabulary, three nice options are';
     embed.addField('Wanikani ($)', 'https://www.wanikani.com/', true);
     embed.addField('Memrise', 'https://www.memrise.com/', true);
     embed.addField('Anki', 'http://ankisrs.net/', true);
   } else if (content == 'ime') {
-    embed.title = 'Kana <:externallink:437129837149159435>';
+    embed.title = '__**IME <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#software';
     embed.description = 'You need to have a software to type in Japanese';
     embed.addField('How to install a Japanese Keyboard', 'https://www.tofugu.com/japanese/how-to-install-japanese-keyboard/', false);
     embed.addField('Google IME', 'https://tools.google.com/dlpage/japaneseinput/eula.html?platform=win', false);
   } else if (content == 'dictionary') {
-    embed.title = 'Kana <:externallink:437129837149159435>';
+    embed.title = '__**Dictionaries <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#dictionary';
     embed.description = 'For translating, avoid using Google Translate';
     embed.addField('Jisho.org', 'http://jisho.org/', true);
     embed.addField('Tangorin', 'http://tangorin.com/', true);
     embed.addField('Weblio', 'http://www.weblio.jp/', true);
   } else {
-    embed.title = 'New to Japanese? Start here!';
+    embed.title = '__**New to Japanese? Start here! <:externallink:437129837149159435>**__';
     embed.url = 'https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#beginner-guide';
     // embed.description = '__**[Complete Guide <:externallink:437129837149159435>](https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md#beginner-guide)**__';
   }
