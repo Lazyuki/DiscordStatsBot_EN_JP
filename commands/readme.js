e28f9997a4fd231ed3e7e664a5bbe9d0ebc420ad
Yuki Ito <yukiito@uw.edu>
2018-04-29 10:27:52 -0700

    android?

--- commands/readme.js
+++ commands/readme.js
@@ -17,7 +17,7 @@ module.exports.command = (message) => {
   let mentioned = message.mentions.members.first();
   let embed = new Discord.RichEmbed();
   embed.title = `**WELCOME ${mentioned ? mentioned.user.username: ''}!! 🎉 READ ME!**`;
-  embed.description = '__**[Japanese Starting Guide<:externallink:438354612379189268>](https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md)**__';
+  embed.description = '__**[Japanese Starting Guide](https://github.com/ryry013/Awesome-Japanese/blob/master/readme.md)**__<:externallink:438354612379189268>';
   embed.setImage('https://i.imgur.com/Wzy5eQD.png');
   //embed.addField('Ask Japanese Questions', '<#189601264424714241>', true);
   //embed.addField('Beginner Japanese Chat', '<#208118574974238721>', true);
