b53fb7ba75bfe90b6c083a1f9915c09ad8585470
Yuki Ito <yukiito@uw.edu>
2018-05-03 18:50:07 -0700

    clearer voicemute message

--- commands/voicemute.js
+++ commands/voicemute.js
@@ -23,8 +23,14 @@ module.exports.command = async (message, content) => {
   for (let [ , member] of targets) {
     await member.setMute(true, `by ${message.author.tag} Reason: ${reason}` );
     await member.addRole('357687893566947329'); // Voice mute role
-    await member.send(`You have been voice muted in the English-Japanese Language Exchange server for: ${reason}. Contact one of the mods if you need to discuss it.`);
     let embed = new Discord.RichEmbed();
+    embed.title = `You have been voice muted in the English-Japanese Language Exchange server by ${message.author.tag}`;
+    embed.description = `Reason: ${reason}`;
+    embed.color = Number('0xEC891D');
+    embed.setFooter('Contact one of the mods if you need to discuss this issue.', message.author.avatarURL);
+    embed.timestamp = new Date();
+    await member.send({embed});
+    embed = new Discord.RichEmbed();
     embed.setAuthor(`${member.user.tag} has been muted in voice chat` , member.user.avatarURL);
     embed.description = `Reason: ${reason}`;
     embed.color = Number('0xEC891D');
