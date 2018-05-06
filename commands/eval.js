796e26d3587c7ff4a2914d309a5b963d1dc066d6
Yuki Ito <yukiito@uw.edu>
2018-05-06 01:42:15 -0700

    eval available anywhere

--- commands/eval.js
+++ commands/eval.js
@@ -5,11 +5,10 @@ module.exports.alias = [
 ];
 
 module.exports.isAllowed = (message, server, bot) => {
-  if (message.guild.id != '293787390710120449') return false;  // My server  
   return message.author.id == bot.owner_ID;
 };
 
-module.exports.help = 'Eval. Can access (message, content, bot, server)';
+module.exports.help = '__Owner only__: Eval. Can access message, content, bot, server. Send to the channel with `send()`';
 
 module.exports.command = async (message, content, bot, server) => {
   let send = (str) => message.channel.send(str);
