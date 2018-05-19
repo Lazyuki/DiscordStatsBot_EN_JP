c8bbbd896c9b1ac2a093905bb5fdee99c676c137
Yuki Ito <yukiito@uw.edu>
2018-05-19 05:58:55 -0700

    elsar

--- commands/editlsar.js
+++ commands/editlsar.js
@@ -5,7 +5,7 @@ module.exports.alias = [
 ];
 
 module.exports.isAllowed = (message, server, bot) => {
-  if (message.guild.id != '293787390710120449') return false;  // My server    
+  // if (message.guild.id != '293787390710120449') return false;  // My server    
   return message.author.id == bot.owner_ID; // Or Admins?
 };
 
