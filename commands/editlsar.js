721d42430244f7cf29ca7f3856965273ef33a416
Yuki Ito <yukiito@uw.edu>
2018-12-22 02:38:47 -0500

    fix elsar

--- commands/editlsar.js
+++ commands/editlsar.js
@@ -4,16 +4,27 @@ module.exports.alias = [
   'elsar'
 ];
 
-module.exports.isAllowed = (message, server, bot) => {
+module.exports.isAllowed = (message) => {
   // if (message.guild.id != '293787390710120449') return false;  // My server  
   return message.member.hasPermission('ADMINISTRATOR');
   // return message.author.id == bot.owner_ID; // Or Admins?
 };
 
-module.exports.help = '__Mods Only__ Edit the list of self-assignable roles in #server_rules.';
+module.exports.help = '__Mods Only__ Edits the list of self-assignable roles in the lsar stickied channel. Checks for the last message sent by Ciri in the channel';
 
 module.exports.command = async (message, content, bot, server) => {
-  let server_rules = server.guild.channels.get('189585230972190720');
-  let msg = await server_rules.messages.fetch('477007517084418050');
-  msg.edit(content); // USE str for auto change
+  let stickiedChannel = server.guild.channels.get(server.sticky);
+  if (!stickiedChannel) {
+    message.channl.send('Cannot find the sticked channel. Try `,h sticky` first. ');
+    return;
+  }
+
+  let msgs = await stickiedChannel.messages.fetch();
+  let lsar = msgs.filter(msg => msg.author.id === bot.id).first();
+  if (!lsar) {
+    message.channl.send('Cannot find Ciri\'s message');
+    return;
+  }
+  lsar.edit(content); // USE str for auto change
+  message.channl.send('Message edited!');
 };
