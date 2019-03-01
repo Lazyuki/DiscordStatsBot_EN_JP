f0e18566622eae3262869aaa3be033a1830aa1ed
Yuki Ito <yukiito@uw.edu>
2019-02-28 23:32:26 -0500

    all people

--- commands/languages.js
+++ commands/languages.js
@@ -21,6 +21,23 @@ module.exports.command = async (message, content, bot, server) => {
   if (min !== 0 && !min) {
     min = 100;
   }
+  if (min < 0) {
+    const members = server.guild.members;
+    const langs = [0, 0, 0, 0, 0, 0];
+    for (let mem of members.values()) {
+      if (mem) {
+        const roles = mem.roles;
+        if (roles.has(NE)) ++langs[0];
+        if (roles.has(NJ)) ++langs[1];
+        if (roles.has(FE)) ++langs[2];
+        if (roles.has(FJ)) ++langs[3];
+        if (roles.has(OL)) ++langs[4];
+        ++langs[5];
+      }
+    }
+    message.channel.send(`Out of ${langs[5]} people,\n${langs[0]} are Native English\n${langs[1]} are Native Japanese\n${langs[2]} are Fluent English\n${langs[3]} are Fluent Japanese\n${langs[4]} are Other Language`);
+    return;
+  }
   const users = server.users;
   const langs = [0, 0, 0, 0, 0, 0];
   for (let user in users) {
