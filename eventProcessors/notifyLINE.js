9ab40af407b9007a53e510081917776f0aae3e75
Yuki Ito <yukiito@uw.edu>
2019-03-12 04:09:49 -0400

    add riri Line notify

--- eventProcessors/notifyLINE.js
+++ eventProcessors/notifyLINE.js
@@ -1,12 +1,14 @@
 module.exports.name = 'notifyLINE';
 module.exports.events = ['NEW'];
 const ryryID = '202995638860906496';
+const ririID = '282243864771821568';
+const activeStaff = '240647591770062848';
 const config = require('../config.json');
 const request = require('request');
 
 module.exports.isAllowed = (message, server, bot) => {
-  return message.mentions.users.has(bot.owner_ID) || message.mentions.roles.has('240647591770062848') // active staff
-   || message.mentions.users.has(ryryID);
+  return message.mentions.users.has(bot.owner_ID) || message.mentions.roles.has(activeStaff) // active staff
+   || message.mentions.users.has(ryryID)|| message.mentions.users.has(ririID);
 };
 
 
@@ -28,15 +30,22 @@ function notify(message, token) {
 module.exports.process = async (message, server, bot) => {
   if (message.content.startsWith('t!') || message.content.startsWith('.')) return; // ignore bot commands
   let me = await server.guild.member(bot.owner_ID);
-  let ry = await server.guild.member(ryryID); // Ry
+  let ry = await server.guild.member(ryryID);
+  let riri = await server.guild.member(ririID);
   if (me.presence.status == 'offline'
       &&  (message.mentions.users.has(bot.owner_ID) 
-           || message.mentions.roles.has('240647591770062848'))) { // If I'm offline
+           || message.mentions.roles.has(activeStaff))) { // If I'm offline
     notify(message, config.LINEnotifyToken);
   }
   if ((ry.presence.status == 'offline' && message.mentions.users.has(ryryID))
-      || message.mentions.roles.has('240647591770062848')) {
+      || message.mentions.roles.has(activeStaff)) {
     notify(message, config.ryryLINEnotifyToken);
+  }
+
+  if (riri.presence.status == 'offline'
+      &&  (message.mentions.users.has(ririID) 
+           || message.mentions.roles.has(activeStaff))) { 
+    notify(message, config.ririLINEnotifyToken);
   }
 };
 
