6a8dae59bee225db5a6e16732e7efd102a6222a9
Yuki Ito <yukiito@uw.edu>
2019-04-02 17:43:14 +0900

    watched

--- commands/watched.js
+++ commands/watched.js
@@ -12,5 +12,5 @@ module.exports.help = 'Shows users on the watchlist.';
 module.exports.command = async (message, content, bot, server) => {
   let res = `Watched users (${server.watchedUsers.length}):\n`;
   res += server.watchedUsers.map(u => `<@${u}>`).join(', ');
-  message.channel.send(res, {split: true});
+  message.channel.send(res, {split: { maxLength: 1950, char: ',' }});
 };
