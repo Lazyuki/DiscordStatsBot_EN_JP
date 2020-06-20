081cda7978b85a0deaa15e6803c3ccdf38daa094
Yuki Ito <yukiito@uw.edu>
2020-06-20 17:51:34 -0400

    more langs

--- commands/languages.js
+++ commands/languages.js
@@ -23,7 +23,7 @@ module.exports.command = async (message, content, bot, server) => {
   }
   if (min < 0) {
     const members = server.guild.members;
-    const langs = [0, 0, 0, 0, 0, 0, 0, 0];
+    const langs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     for (let mem of members.values()) {
       if (mem) {
         const roles = mem.roles;
@@ -34,14 +34,17 @@ module.exports.command = async (message, content, bot, server) => {
         if (roles.has(OL)) ++langs[4];
         if (roles.has(NE) && roles.has(NJ)) ++langs[5];
         if (roles.has(NJ) && roles.has(FE)) ++langs[6];
-        ++langs[7];
+        if (roles.has(NJ) && roles.has(OL)) ++langs[7];
+        if (roles.has(NE) && roles.has(OL)) ++langs[8];
+        if (roles.has(FJ) && roles.has(FE)) ++langs[9];
+        ++langs[10];
       }
     }
-    message.channel.send(`Out of ${langs[7]} people,\n${langs[0]} are Native English\n${langs[1]} are Native Japanese\n${langs[2]} are Fluent English\n${langs[3]} are Fluent Japanese\n${langs[4]} are Other Language\n${langs[5]} are both NE and NJ\n${langs[6]} are NJ and FE`);
+    message.channel.send(`Out of ${langs[7]} people,\n${langs[0]} are Native English\n${langs[1]} are Native Japanese\n${langs[2]} are Fluent English\n${langs[3]} are Fluent Japanese\n${langs[4]} are Other Language\n${langs[5]} are both NE and NJ\n${langs[6]} are NJ and FE\n${langs[7]} are NJ and OL\n${langs[8]} are NE and OL\n${langs[9]} are FJ and FE`);
     return;
   }
   const users = server.users;
-  const langs = [0, 0, 0, 0, 0, 0, 0, 0];
+  const langs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   for (let user in users) {
     let res = users[user].totalStats();
     if (res >= min) {
@@ -55,9 +58,12 @@ module.exports.command = async (message, content, bot, server) => {
         if (roles.has(OL)) ++langs[4];
         if (roles.has(NE) && roles.has(NJ)) ++langs[5];
         if (roles.has(NJ) && roles.has(FE)) ++langs[6];
-        ++langs[7];
+        if (roles.has(NJ) && roles.has(OL)) ++langs[7];
+        if (roles.has(NE) && roles.has(OL)) ++langs[8];
+        if (roles.has(FJ) && roles.has(FE)) ++langs[9];
+        ++langs[10];
       }
     }
   }
-  message.channel.send(`Out of ${langs[7]} people who have sent more than ${min} messages in the past 30 days,\n${langs[0]} are Native English\n${langs[1]} are Native Japanese\n${langs[2]} are Fluent English\n${langs[3]} are Fluent Japanese\n${langs[4]} are Other Language\n${langs[5]} are both NE and NJ\n${langs[6]} are NJ and FE`);
+  message.channel.send(`Out of ${langs[10]} people who have sent more than ${min} messages in the past 30 days,\n${langs[0]} are Native English\n${langs[1]} are Native Japanese\n${langs[2]} are Fluent English\n${langs[3]} are Fluent Japanese\n${langs[4]} are Other Language\n${langs[5]} are both NE and NJ\n${langs[6]} are NJ and FE\n${langs[7]} are NJ and OL\n${langs[8]} are NE and OL\n${langs[9]} are FJ and FE`);
 };
