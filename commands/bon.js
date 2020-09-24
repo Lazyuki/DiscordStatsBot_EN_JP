3700d5115aba700f74d5048f1c5041f13691bec0
Yuki Ito <yukiito@uw.edu>
2020-09-23 21:49:42 -0400

    update bon

--- commands/bon.js
+++ commands/bon.js
@@ -9,7 +9,7 @@ module.exports.isAllowed = (message, server) => {
   return true;
 };
 
-module.exports.help = '`,bon` Bonyari#4649\'s guide';
+module.exports.help = '`,bon` Bonyari#8184\'s guide';
 const Discord = require('discord.js');
 const externalLink = '<:externallink:438354612379189268>';
 const link = 'https://docs.google.com/document/d/19FEIOJWbLhJQ-AmepxFBMC2ebhJJr9RBUMfMeatYuq8/edit?usp=sharing';
@@ -18,15 +18,15 @@ module.exports.command = async (message, content, bot, server) => {
   message.delete();
   const embed = new Discord.RichEmbed();
   let bon;
-   try {
-     bon = await server.guild.fetchMember('149169203910279179');
-   } catch {
-     console.log('failed to fetch bon');
-   }
+  try {
+    bon = await server.guild.fetchMember('149169203910279179');
+  } catch (e) {
+    console.log('failed to fetch bon');
+  }
 
   embed.title = `**HOW TO LEARN JAPANESE EFFICIENTLY** ${externalLink}`;
   embed.url = link;
-  embed.setFooter('Written by Bonyari#4649', bon ? bon.user.avatarURL : '');
+  embed.setFooter(`Written by ${bon.user.tag}`, bon ? bon.user.avatarURL : '');
   embed.color = 8843151;
   message.channel.send({embed});
 };
