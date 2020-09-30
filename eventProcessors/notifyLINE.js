4b8679c764b71e64e11ee3394ed010b9209853d5
Yuki Ito <yukiito@uw.edu>
2020-09-30 01:24:58 -0400

    new line

--- eventProcessors/notifyLINE.js
+++ eventProcessors/notifyLINE.js
@@ -1,26 +1,36 @@
-module.exports.name = 'notifyLINE';
-module.exports.events = ['NEW'];
-const ryryID = '202995638860906496';
-const ririID = '282243864771821568';
-const activeStaff = '240647591770062848';
-const config = require('../config.json');
-const request = require('request');
+module.exports.name = "notifyLINE";
+module.exports.events = ["NEW"];
+const ryryID = "202995638860906496";
+const ririID = "282243864771821568";
+const zappiID = "652705051684503557";
+const skyzID = "107202830846148608";
+const IDs = [ryryID, ririID, zappiID, skyzID];
+
+const activeStaff = "240647591770062848";
+const config = require("../config.json");
+const request = require("request");
 
 module.exports.isAllowed = (message, server, bot) => {
-  return message.mentions.users.has(bot.owner_ID) || message.mentions.roles.has(activeStaff) // active staff
-   || message.mentions.users.has(ryryID)|| message.mentions.users.has(ririID);
+  const mentions = message.mentions.users;
+  return (
+    mentions.has(bot.owner_ID) ||
+    message.mentions.roles.has(activeStaff) || // active staff
+    IDs.some((id) => mentions.has(id))
+  );
 };
 
-
 function notify(message, token) {
-  let options = { method: 'POST',
-    url: 'https://notify-api.line.me/api/notify',
-    headers:
-        {
-          'authorization': `Bearer ${token}`,
-          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-        },
-    formData: {message: `#${message.channel.name}\n@${message.author.username}:\n${message.cleanContent}`}
+  const options = {
+    method: "POST",
+    url: "https://notify-api.line.me/api/notify",
+    headers: {
+      authorization: `Bearer ${token}`,
+      "content-type":
+        "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+    },
+    formData: {
+      message: `#${message.channel.name}\n${message.author.username}:\n${message.cleanContent}`,
+    },
   };
   request(options, function (error) {
     if (error) console.log(error);
@@ -28,24 +38,45 @@ function notify(message, token) {
 }
 
 module.exports.process = async (message, server, bot) => {
-  if (message.content.startsWith('t!') || message.content.startsWith('.')) return; // ignore bot commands
-  let me = await server.guild.member(bot.owner_ID);
-  let ry = await server.guild.member(ryryID);
-  let riri = await server.guild.member(ririID);
-  if (me.presence.status == 'offline'
-      &&  (message.mentions.users.has(bot.owner_ID) 
-           || message.mentions.roles.has(activeStaff))) { // If I'm offline
+  if (message.content.startsWith("t!") || message.content.startsWith("."))
+    return; // ignore bot commands
+  const me = await server.guild.member(bot.owner_ID);
+  const ry = await server.guild.member(ryryID);
+  const riri = await server.guild.member(ririID);
+  const zappi = await server.guild.member(zappiID);
+  const skyz = await server.guild.member(skyzID);
+
+  if (
+    me.presence.status == "offline" &&
+    (message.mentions.users.has(bot.owner_ID) ||
+      message.mentions.roles.has(activeStaff))
+  ) {
+    // If I'm offline
     notify(message, config.LINEnotifyToken);
   }
-  if ((ry.presence.status == 'offline' && message.mentions.users.has(ryryID))
-      || message.mentions.roles.has(activeStaff)) {
+  if (
+    (ry.presence.status == "offline" && message.mentions.users.has(ryryID)) ||
+    message.mentions.roles.has(activeStaff)
+  ) {
     notify(message, config.ryryLINEnotifyToken);
   }
 
-  if (riri.presence.status == 'offline'
-      &&  (message.mentions.users.has(ririID) 
-           || message.mentions.roles.has(activeStaff))) { 
+  if (
+    riri.presence.status == "offline" &&
+    (message.mentions.users.has(ririID) ||
+      message.mentions.roles.has(activeStaff))
+  ) {
     notify(message, config.ririLINEnotifyToken);
   }
-};
 
+  if (message.mentions.roles.has(activeStaff)) {
+    notify(message, config.zappiLINEnotifyToken);
+  }
+
+  if (
+    skyz.presence.status === "offline" &&
+    message.mentions.roles.has(activeStaff)
+  ) {
+    notify(message, config.skyzLINEnotifyToken);
+  }
+};
