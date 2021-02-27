c5ea4910e8952cca79ce06f85db3bfe76ec742a3
Yuki Ito <yukiito@uw.edu>
2021-02-27 13:20:18 -0500

    allow warn anywhere

--- commands/warn.js
+++ commands/warn.js
@@ -10,7 +10,12 @@ module.exports.initialize = (json, server) => {
 };
 
 module.exports.isAllowed = (message, server) => {
-  return server.hiddenChannels.includes(message.channel.id);
+  return (
+    server.hiddenChannels.includes(message.channel.id) ||
+    (server.guild.id === '189571157446492161' &&
+      (message.member.hasPermission('BAN_MEMBERS') ||
+        message.member.roles.cache.has('543721608506900480')))
+  );
 };
 
 module.exports.help =
