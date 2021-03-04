fd58bef7e44cf39cf0faefcf71b625e6073fb165
Yuki Ito <yukiito@uw.edu>
2021-03-03 23:20:01 -0500

    embed

--- commands/fe.js
+++ commands/fe.js
@@ -7,9 +7,12 @@ module.exports.isAllowed = (message, server) => {
 
 module.exports.help = '`,fe` explanation of fe';
 
+const Discord = require('discord.js');
 module.exports.command = async (message) => {
-  message.channel.send(
-    "People with the 'Fluent' tag must set a good example for English learners. If we see you speak accurately and actively in English, you will receive the tag (may take a while)."
-  );
+  const embed = new Discord.MessageEmbed();
+  embed.description =
+    "People with the 'Fluent' tag must set a good example for English learners. If we see you speak accurately and actively in English, you will receive the tag (may take a while).";
+  embed.color = Number('0x3995ff');
+  message.channel.send({ embed });
   message.delete();
 };
--- commands/fj.js
+++ commands/fj.js
@@ -7,9 +7,13 @@ module.exports.isAllowed = (message, server) => {
 
 module.exports.help = '`,fj` explanation of fj';
 
+const Discord = require('discord.js');
 module.exports.command = async (message) => {
-  message.channel.send(
-    '深緑(日本語が流暢)のタグを得るためには、まずは管理者かWelcoming Partyの日本人が、あなたのテキストを読んで流暢だと認める必要があります。FJタグご希望の方は、ボイスチャット上でも日本語が流暢であることを確認させてもらいます。'
-  );
+  const embed = new Discord.MessageEmbed();
+  embed.description =
+    '深緑(日本語が流暢)のタグを得るためには、まずは管理者かWelcoming Partyの日本人が、あなたのテキストを読んで流暢だと認める必要があります。FJタグご希望の方は、ボイスチャット上でも日本語が流暢であることを確認させてもらいます。\n' +
+    "If you don't understand this, you aren't ready yet.";
+  embed.color = Number('0x279b4a');
+  message.channel.send({ embed });
   message.delete();
 };
