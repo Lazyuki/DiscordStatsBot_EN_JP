afcf47a4a2dddbacef23163cd463b08584bf3cbe
Yuki Ito <yukiito@uw.edu>
2021-05-17 01:38:03 -0400

    configurable selfmute

--- commands/selfMute.js
+++ commands/selfMute.js
@@ -1,3 +1,4 @@
+const Util = require('../classes/Util.js');
 module.exports.name = 'selfMute';
 module.exports.alias = ['selfmute', 'sm'];
 
@@ -13,7 +14,7 @@ async function unmute(user_id, server) {
     }
   }
   try {
-    await member.roles.remove([CHAT_MUTED, VOICE_BANNED]);
+    await member.roles.remove(server.selfmuteRoles);
     delete server.selfmutes[user_id];
   } catch (e) {
     console.error(`Failed to remove selfmute roles for ${user_id}`, e);
@@ -25,30 +26,36 @@ async function unmute(user_id, server) {
 
 module.exports.initialize = (json, server) => {
   server.selfmutes = {};
-  if (!json || !json['selfmutes']) return;
-  server.selfmutes = json['selfmutes'];
-  for (const user_id in server.selfmutes) {
-    const time = server.selfmutes[user_id];
-    setTimeout(
-      () => unmute(user_id, server),
-      new Date(time).getTime() - new Date().getTime()
-    );
+  server.selfmuteRoles = [];
+  if (!json) return;
+  if (json['selfmutes']) {
+    server.selfmutes = json['selfmutes'];
+    for (const user_id in server.selfmutes) {
+      const time = server.selfmutes[user_id];
+      setTimeout(
+        () => unmute(user_id, server),
+        new Date(time).getTime() - new Date().getTime()
+      );
+    }
+  }
+  if (json['selfmuteRoles']) {
+    server.selfmuteRoles = json['selfmuteRoles'];
   }
 };
 
 module.exports.isAllowed = (message, server) => {
-  return server.guild.id === '189571157446492161';
+  return (
+    server.guild.id === '189571157446492161' ||
+    (server.selfmuteRoles && server.selfmuteRoles.length)
+  );
 };
 
 module.exports.help =
   "Mute yourself (text and voice) in the server. Mods can't unmute you so don't message them. `,selfmute 1d20h43m4s`";
 
-const BLIND = '645021058184773643';
-const CHAT_MUTED = '259181555803619329';
-const VOICE_BANNED = '327917620462354442';
-const VOICE_MUTED = '357687893566947329';
 const TIME_REGEX = /([0-9]+d)?([0-9]+h)?([0-9]+m)?([0-9]+s)?/;
 const SECRET_REGEX = /remove <?@?!?([0-9]+)>?/;
+const SETTINGS_REGEX = /set (?:<?@?&?([0-9]+)>? ?)+/;
 
 module.exports.command = async (message, content, bot, server) => {
   let matches = SECRET_REGEX.exec(content);
@@ -58,10 +65,21 @@ module.exports.command = async (message, content, bot, server) => {
     message.channel.send(`✅  Unselfmuted`);
     return;
   }
+
+  matches = SETTINGS_REGEX.exec(content);
+  if (matches && message.member.hasPermission('ADMINISTRATOR')) {
+    const discordIDs = content.match(Util.REGEX_RAW_ID);
+    server.selfmuteRoles = discordIDs;
+    message.channel.send(
+      `✅ Set selfmute roles to ${discordIDs.map((d) => `<@&${d}>`).join(', ')}`
+    );
+    return;
+  }
+
   matches = TIME_REGEX.exec(content);
   if (!matches) {
     message.channel.send(
-      'Invalid time syntax. Only `d`, `h`, `m`, and `s` are supported'
+      'Invalid time syntax. Only `d`, `h`, `m`, and `s` are supported. '
     );
     return;
   }
@@ -95,7 +113,7 @@ module.exports.command = async (message, content, bot, server) => {
   server.selfmutes[member.id] = unmuteDateMillis;
   server.save();
 
-  await message.member.roles.add([CHAT_MUTED, VOICE_BANNED], 'Selfmuted');
+  await message.member.roles.add(server.selfmuteRoles, 'Selfmuted');
   setTimeout(() => unmute(member.id, server), totalMillis);
 
   message.channel.send(
