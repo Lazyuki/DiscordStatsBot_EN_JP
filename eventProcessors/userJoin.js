c881235c1613e93d1d8978c3be61b028890729d0
Yuki Ito <yukiito@uw.edu>
2021-06-12 20:34:06 -0400

    fix invite

--- eventProcessors/userJoin.js
+++ eventProcessors/userJoin.js
@@ -240,9 +240,9 @@ async function postLogs(member, server) {
     JHO.send(welcome);
   } else {
     if (member.user.username.toLowerCase().includes('twitter.com')) return;
-    if (member.guild.member(member.id).roles.cache.has('852380454546964490'))
-      return; // Stage Visitor
     setTimeout(async () => {
+      if (member.guild.member(member.id).roles.cache.has('852380454546964490'))
+        return; // Stage Visitor
       let msgs = await JHO.messages.fetch({ limit: 50 });
       for (let [, msg] of msgs) {
         if (
