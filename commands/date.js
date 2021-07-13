5c606326ecae417745bdb8e8100204c7b0d654f1
Yuki Ito <yukiito@uw.edu>
2021-07-12 23:15:31 -0400

    date

---
+++ commands/date.js
@@ -0,0 +1,13 @@
+module.exports.name = 'date';
+
+module.exports.alias = ['date'];
+
+module.exports.isAllowed = () => {
+  return true;
+};
+
+module.exports.help = '';
+
+module.exports.command = () => {};
+
+module.exports.isCirillaCommand = true;
