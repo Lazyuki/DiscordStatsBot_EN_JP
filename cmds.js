c4c1e540e7301148333e39106af5e02ec37c444a
Yuki Ito <layzuki@gmail.com>
2022-04-03 21:56:57 +0100

    fix migrate

--- cmds.js
+++ cmds.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
-const init = require('./init.json');
 
 let commands = {};
 let inits = [];
-let commandNames = {}
-const migratedCommands = init.migratedCommands || []
+let commandNames = {};
 
 fs.readdir('./commands/', (err, files) => {
   files.forEach((file) => {
     if (!file.endsWith('.js')) return;
     let command = require(`./commands/${file}`);
     if (command.initialize) inits.push(command.initialize);
-    if (migratedCommands.includes(command.name)) {
-      command.isCirillaCommand = true;
-    }
     commandNames[command.name] = command;
     command.alias.forEach((name) => {
       if (commands[name]) throw new Error(`The alias ${name} is already used.`);
