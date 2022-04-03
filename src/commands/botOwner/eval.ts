c828e5d74a3d746027ee9a03f17b5a60632472ae
Yuki Ito <layzuki@gmail.com>
2022-04-03 21:07:43 +0100

    eval with stringify

--- src/commands/botOwner/eval.ts
+++ src/commands/botOwner/eval.ts
@@ -1,11 +1,11 @@
-import { makeEmbed, successEmbed } from '@utils/embed';
+import { errorEmbed, makeEmbed, successEmbed } from '@utils/embed';
 import { BotCommand } from '@/types';
 
 const command: BotCommand = {
   name: 'eval',
   isAllowed: ['BOT_OWNER'],
   description:
-    'Can access [message, content, bot, server, send, makeEmbed]. Send to the channel with `send()` or simply return',
+    'Can access [message, content, bot, server, send, makeEmbed, str]. Send to the channel with `send()` or simply return',
   examples: [
     'eval return message.guild.memberCount',
     'eval send(makeEmbed({ description: "Hello" }))',
@@ -21,16 +21,28 @@ const command: BotCommand = {
         'server',
         'send',
         'makeEmbed',
+        'str',
         evalContent
       );
-      const ret = await func(content, message, bot, server, send, makeEmbed);
+      const str = (obj: object) =>
+        typeof obj === 'object' ? JSON.stringify(obj, null, 4) : obj;
+      const ret = await func(
+        content,
+        message,
+        bot,
+        server,
+        send,
+        makeEmbed,
+        str
+      );
       if (ret !== undefined) {
         await send(successEmbed(String(ret) || '*empty string*'));
       } else {
         await message.react('✅');
       }
     } catch (e) {
-      await reply((e as Error).message);
+      const err = e as Error;
+      await reply(errorEmbed(`${err.name}: ${err.message}`));
     }
   },
 };
