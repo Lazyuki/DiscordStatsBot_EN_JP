3adcf34a2b5d2b02ac3948d1a71014b80eeea3b7
Yuki Ito <layzuki@gmail.com>
2022-04-10 23:12:33 +0100

    safer delete

--- src/utils/safeDelete.ts
+++ src/utils/safeDelete.ts
@@ -3,7 +3,11 @@ import { GuildMessage } from '@/types';
 
 export function safeDelete(message: GuildMessage) {
   const me = message.guild.me;
-  if (me && message.channel.permissionsFor(me).has('MANAGE_MESSAGES')) {
+  if (!me) return;
+  if (
+    me.permissions.has('MANAGE_MESSAGES') ||
+    message.channel.permissionsFor(me)?.has('MANAGE_MESSAGES')
+  ) {
     setTimeout(() => message.delete().catch((e) => {}), 200); // Delay a little to let Discord UI actually delete the message
   }
 }
