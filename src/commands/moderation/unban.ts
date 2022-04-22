2917c897696b7b602bc85a04802497421359c4ac
Yuki Ito <layzuki@gmail.com>
2022-04-22 02:04:45 +0100

    make sure cached banned users can be unbanned

--- src/commands/moderation/unban.ts
+++ src/commands/moderation/unban.ts
@@ -16,13 +16,22 @@ const unban: BotCommand = {
       content,
       server.guild
     );
-    if (members.length) {
+    if (members.length && members.every((m) => m.joinedAt)) {
       throw new CommandArgumentError(
         `${members.join(', ')} already in the server.`
       );
     }
     if (nonMemberIds.length === 0) {
-      throw new CommandArgumentError('Please specify IDs of users to unban');
+      if (members.length) {
+        // member is still cached
+        members.forEach((m) => {
+          if (!m.joinedAt) {
+            nonMemberIds.push(m.id);
+          }
+        });
+      } else {
+        throw new CommandArgumentError('Please specify IDs of users to unban');
+      }
     }
     const reason = `By ${message.author.tag} (${message.author.id}). Reason: ${
       restContent || 'Unspecified'
