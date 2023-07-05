30bf9e3c7d71ff265862abf8ae8159c39646dc4f
Yuki Ito <layzuki@gmail.com>
2023-07-05 02:26:35 +0100

    update with install

--- src/commands/botOwner/update.ts
+++ src/commands/botOwner/update.ts
@@ -9,7 +9,15 @@ const command: BotCommand = {
   name: 'update',
   isAllowed: ['BOT_OWNER'],
   description: 'Pull from git and restart the bot',
-  normalCommand: async ({ bot, message, ...rest }) => {
+  options: [
+    {
+      name: 'install',
+      short: 'i',
+      description: 'Run `npm install` after git pull and then restart',
+      bool: true,
+    },
+  ],
+  normalCommand: async ({ bot, message, options, ...rest }) => {
     const exec = util.promisify(execPromise);
     const gitpullCommand = `git pull`;
     let stdoutMessage = await message.channel.send(
@@ -43,6 +51,40 @@ const command: BotCommand = {
         )
       );
       return;
+    }
+    if (options['install']) {
+      const npmInstallCommand = 'npm install';
+      try {
+        stdoutMessage = await message.channel.send(
+          codeBlock('$ ' + npmInstallCommand)
+        );
+        await message.channel.sendTyping();
+        const npmInstall = await exec(npmInstallCommand);
+        stdoutMessage = await stdoutMessage.edit(
+          appendCodeBlock(stdoutMessage.content, npmInstall.stdout, 2000)
+        );
+        if (
+          npmInstall.stderr &&
+          npmInstall.stderr.toLowerCase().includes('error')
+        ) {
+          await message.channel.send(
+            errorEmbed(
+              `Error during \`${gitpullCommand}\`:\n${codeBlock(
+                npmInstall.stderr
+              )}`
+            )
+          );
+          return;
+        }
+      } catch (e) {
+        const err = e as Error;
+        await message.channel.send(
+          errorEmbed(
+            `Error during \`${npmInstallCommand}\`:\n${err.name}: ${err.message}`
+          )
+        );
+        return;
+      }
     }
     // Pulled something, so restart the bot
     await bot.commands['restart'].normalCommand?.({
